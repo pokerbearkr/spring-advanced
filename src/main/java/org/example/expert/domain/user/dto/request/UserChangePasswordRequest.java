6d5facd498757374fb44fa02902d6428a9cae02e
pokerbearkr <ogdongwon@gmail.com>
2025-04-15 23:59:49 +0900

    refactor : 비밀번호 검증 Dto에서 처리

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,17 +1,16 @@
 package org.example.expert.domain.user.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
     private String oldPassword;
-    @NotBlank
+
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(regexp = ".*\\d.*", message = "비밀번호에는 숫자가 포함되어야 합니다.")
+    @Pattern(regexp = ".*[A-Z].*", message = "비밀번호에는 대문자가 포함되어야 합니다.")
     private String newPassword;
 }
